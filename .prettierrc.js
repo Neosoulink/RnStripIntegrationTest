336ccb25b9aa56a84b79a28cc7c490c04bde8f64
Neosoulink <neodarksoulink@gmail.com>
2021-11-17 08:02:09 +0100

    Setup project

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,4 @@
 module.exports = {
-  bracketSpacing: false,
-  jsxBracketSameLine: true,
-  singleQuote: true,
   trailingComma: 'all',
   arrowParens: 'avoid',
 };
--- App.js
+++ App.js
@@ -1,112 +1,29 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
-import type {Node} from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
+	SafeAreaView,
+	ScrollView,
+	StatusBar,
+	StyleSheet,
+	Text,
+	View,
 } from 'react-native';
+import { StripeProvider } from '@stripe/stripe-react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
+const App = () => {
+	return <StripeProvider
+		publishableKey="pk_test_51Hzl5jCyrqJewPlXE3kCDtscnhVJLpcgQD6oqaB3tOh7GkBVh72OvQR7ygOfrP8bVAVBOKQ5EnJ59X3JB0NriG7300EFngOXPQ"
+		urlScheme="your-url-scheme" // required for 3D Secure and bank redirects
+		merchantIdentifier="merchant.com.{{YOUR_APP_NAME}}" // required for Apple Pay
+	>
+		<SafeAreaView style={{}}>
+			<StatusBar translucent={false} backgroundColor="transparent" />
+			<ScrollView contentInsetAdjustmentBehavior="automatic" style={{}}>
+				<View style={{}}>
+					<Text>Text</Text>
+				</View>
+			</ScrollView>
+		</SafeAreaView>
+	</StripeProvider>;
 };
-
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
